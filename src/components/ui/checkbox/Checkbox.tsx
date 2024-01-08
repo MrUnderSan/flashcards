b8e236ac6024953d6284718af7f6e28ae9ff71fb
snakiso <igorchagov@mail.ru>
2024-01-08 14:32:06 +0300

    feat: add checkbox component

---
+++ src/components/ui/checkbox/Checkbox.tsx
@@ -0,0 +1,47 @@
+import { Check } from "@/assets/icons/Check";
+import { Typography } from "@/components/ui/typography";
+import * as CheckboxRadix from "@radix-ui/react-checkbox";
+
+import s from "./checkbox.module.scss";
+
+export type CheckboxProps = {
+  checked?: boolean;
+  className?: string;
+  disabled?: boolean;
+  id?: string;
+  label?: string;
+  onChange?: (checked: boolean) => void;
+  required?: boolean;
+};
+
+export const Checkbox = (props: CheckboxProps) => {
+  const { checked, className, disabled, id, label, onChange, required } = props;
+
+  return (
+    <div className={s.container}>
+      <CheckboxRadix.Root
+        checked={checked}
+        className={`${s.checkbox} ${className ? className : ""}`}
+        defaultChecked
+        disabled={disabled}
+        id={id}
+        onCheckedChange={onChange}
+        required={required}
+      >
+        <CheckboxRadix.Indicator>
+          <Check />
+        </CheckboxRadix.Indicator>
+      </CheckboxRadix.Root>
+      {label && (
+        <Typography
+          as={"label"}
+          className={s.label}
+          htmlFor={id}
+          variant={"body2"}
+        >
+          {label}
+        </Typography>
+      )}
+    </div>
+  );
+};
