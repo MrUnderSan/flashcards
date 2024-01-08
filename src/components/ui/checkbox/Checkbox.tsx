422627addafca0ffe2845eaf24eacc1871891d4f
snakiso <igorchagov@mail.ru>
2024-01-08 15:04:18 +0300

    fix: checkbox component and add icon for him

--- src/components/ui/checkbox/Checkbox.tsx
+++ src/components/ui/checkbox/Checkbox.tsx
@@ -1,27 +1,27 @@
-import { Check } from "@/assets/icons/Check";
-import { Typography } from "@/components/ui/typography";
-import * as CheckboxRadix from "@radix-ui/react-checkbox";
+import { Check } from '@/assets/icons/check'
+import { Typography } from '@/components/ui/typography'
+import * as CheckboxRadix from '@radix-ui/react-checkbox'
 
-import s from "./checkbox.module.scss";
+import s from './checkbox.module.scss'
 
 export type CheckboxProps = {
-  checked?: boolean;
-  className?: string;
-  disabled?: boolean;
-  id?: string;
-  label?: string;
-  onChange?: (checked: boolean) => void;
-  required?: boolean;
-};
+  checked?: boolean
+  className?: string
+  disabled?: boolean
+  id?: string
+  label?: string
+  onChange?: (checked: boolean) => void
+  required?: boolean
+}
 
 export const Checkbox = (props: CheckboxProps) => {
-  const { checked, className, disabled, id, label, onChange, required } = props;
+  const { checked, className, disabled, id, label, onChange, required } = props
 
   return (
     <div className={s.container}>
       <CheckboxRadix.Root
         checked={checked}
-        className={`${s.checkbox} ${className ? className : ""}`}
+        className={`${s.checkbox} ${className ? className : ''}`}
         defaultChecked
         disabled={disabled}
         id={id}
@@ -33,15 +33,10 @@ export const Checkbox = (props: CheckboxProps) => {
         </CheckboxRadix.Indicator>
       </CheckboxRadix.Root>
       {label && (
-        <Typography
-          as={"label"}
-          className={s.label}
-          htmlFor={id}
-          variant={"body2"}
-        >
+        <Typography as={'label'} className={s.label} htmlFor={id} variant={'body2'}>
           {label}
         </Typography>
       )}
     </div>
-  );
-};
+  )
+}
