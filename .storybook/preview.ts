0611bbaa0170c60e85970c75f4ea4ffb50537a8f
MrUnderSan <MrUnderSan@gmail.com>
2024-01-10 22:27:47 +0300

    feat: add background color on preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,6 +4,19 @@ import '@fontsource/roboto/700.css'
 import '@/styles/index.scss'
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'light',
+          value: '#fff',
+        },
+        {
+          name: 'dark',
+          value: '#000',
+        },
+      ],
+    },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
