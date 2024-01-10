29322847f4a17efce2777307edad324fdcbde1a3
chepeLove <ilya.chepelov@gmail.com>
2024-01-10 15:33:52 +0300

    feat: change typing and remove ts-ignore

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,32 +1,36 @@
-import { ComponentPropsWithoutRef, ElementRef, ElementType, ForwardedRef, forwardRef } from 'react'
+import {
+  ComponentPropsWithoutRef,
+  ElementRef,
+  ElementType,
+  ReactNode,
+  Ref,
+  forwardRef,
+} from 'react'
+
+import { clsx } from 'clsx'
 
 import s from './button.module.scss'
 
-export type ButtonProps<T extends ElementType = 'button'> = {
+export type ButtonProps<T extends ElementType> = {
   as?: T
   fullWidth?: boolean
   variant?: 'link' | 'primary' | 'secondary' | 'tertiary'
 } & ComponentPropsWithoutRef<T>
 
-const PolymorphButton = <T extends ElementType = 'button'>(
+type ButtonWithRef = <T extends ElementType = 'button'>(
   props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>,
-  ref: ElementRef<T>
-) => {
-  const { as: Tag = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+  ref?: Ref<ElementRef<T>>
+) => ReactNode
 
-  return (
-    // @ts-expect-error The TS2322 error indicates a type assignment issue. In this case, the assigned type is not compatible with the expected type.
-    <Tag
-      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
-      ref={ref}
-      {...rest}
-    />
-  )
-}
+export const Button: ButtonWithRef = forwardRef(
+  <T extends ElementType = 'button'>(
+    props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>,
+    ref: Ref<ElementRef<T>>
+  ) => {
+    const { as, className, fullWidth, variant = 'primary', ...rest } = props
+    const Tag: ElementType = as || 'button'
+    const classNames = clsx(s.button, s[variant], fullWidth ? s.fullWidth : '', className)
 
-export const Button = forwardRef(PolymorphButton) as <T extends ElementType = 'button'>(
-  props: ButtonProps<T> &
-    Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>> & {
-      ref?: ForwardedRef<ElementRef<T>>
-    }
-) => ReturnType<typeof PolymorphButton>
+    return <Tag className={classNames} ref={ref} {...rest} />
+  }
+)
