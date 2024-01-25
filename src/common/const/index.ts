2ce74fb0049965954e58b075babe7dae1cc51a42
chepeLove <ilya.chepelov@gmail.com>
2024-01-25 19:46:15 +0300

    feat: delete routes file and add ROUTES,OPTIONS in index

--- src/common/const/index.ts
+++ src/common/const/index.ts
@@ -1,1 +1,19 @@
-export * from './routes'
+export const ROUTES = {
+  base: '/',
+  checkEmail: '/check-email',
+  createNewPassword: '/create-new-password',
+  deck: '/decks/:deckId',
+  decks: '/decks',
+  learn: '/learn',
+  profile: '/profile',
+  recoverPassword: '/recover-password',
+  rest: '/*',
+  signIn: '/sign-in',
+  signUp: '/sign-up',
+} as const
+
+export const SELECT_OPTIONS_PAGINATION = [
+  { title: '5', value: '5' },
+  { title: '10', value: '10' },
+  { title: '15', value: '15' },
+]
