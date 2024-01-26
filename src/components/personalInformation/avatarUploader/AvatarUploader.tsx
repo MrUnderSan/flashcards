fc4829f17cc9a41cc965992b5e4af9a5f14375be
chepeLove <ilya.chepelov@gmail.com>
2024-01-26 18:38:20 +0300

    fix: remove unused validation

--- src/components/personalInformation/avatarUploader/AvatarUploader.tsx
+++ src/components/personalInformation/avatarUploader/AvatarUploader.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef } from 'react'
 
 import { EditAvatar } from '@/assets'
+import { IMAGE_SCHEMA } from '@/common/const'
 import { Avatar } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { FileUploader } from '@/components/ui/fileUploader/FileUploader'
@@ -17,15 +18,7 @@ type AvatarUploaderProps = {
   updateAvatar: (avatar: AvatarUploaderValue) => void
 }
 
-const AvatarUploaderSchema = z
-  .instanceof(File)
-  .refine(file => file.size <= 1000000, `Max image size is 1MB. The file will not be uploaded.`)
-  .refine(
-    file => ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'].includes(file.type),
-    'Only .jpg, .jpeg, .png and .webp formats are supported. The file will not be uploaded.'
-  )
-
-export type AvatarUploaderValue = z.infer<typeof AvatarUploaderSchema>
+export type AvatarUploaderValue = z.infer<typeof IMAGE_SCHEMA>
 
 export const AvatarUploader = ({
   avatarUrl,
@@ -35,39 +28,31 @@ export const AvatarUploader = ({
 }: AvatarUploaderProps) => {
   const fileRef = useRef<HTMLInputElement>(null)
 
-  const [avatar, setAvatar] = useState<File | null>(null)
-
-  const avatarIsValid =
-    avatar !== null &&
-    ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'].includes(avatar.type) &&
-    avatar.size <= 1000000
-
-  const avatarUploaderClasses = clsx(s.root, className)
-
-  useEffect(() => {
+  const updateAvatarHandler = (avatar: File | null) => {
     if (avatar) {
       updateAvatar(avatar)
     }
-  }, [updateAvatar, avatar])
+  }
+
+  const avatarUploaderClasses = clsx(s.root, className)
+
+  const onClickTrigger = () => {
+    fileRef.current?.click()
+  }
 
   return (
     <div className={avatarUploaderClasses}>
-      <Avatar
-        className={s.avatar}
-        src={avatarIsValid ? URL.createObjectURL(avatar) : avatarUrl}
-        title={'avatar'}
-      />
+      <Avatar className={s.avatar} src={avatarUrl} />
       {editable && (
         <FileUploader
           className={s.uploader}
           ref={fileRef}
-          setFile={setAvatar}
+          setFile={updateAvatarHandler}
           trigger={
-            <Button as={'span'} className={s.editAvatar}>
+            <Button className={s.editAvatar} onClick={onClickTrigger}>
               <EditAvatar />
             </Button>
           }
-          validationSchema={AvatarUploaderSchema}
         />
       )}
     </div>
