e87045ee2f86f5df77b2617b230025362da354ac
chepeLove <ilya.chepelov@gmail.com>
2024-01-31 13:37:12 +0300

    feat: add CardsHeader and modals for deck

--- src/pages/deck/Deck.tsx
+++ src/pages/deck/Deck.tsx
@@ -1,19 +1,20 @@
-import { ChangeEvent, useState } from "react";
-import { Link, useParams } from 'react-router-dom'
+import { ChangeEvent, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 import { SELECT_OPTIONS_PAGINATION } from '@/common/const'
 import { useDecksSearchParams } from '@/common/hooks'
 import { useDebounce } from '@/common/hooks/useDebounce'
 import { Cards } from '@/components/cards'
+import { CardsHeader } from '@/components/cards/header/CardsHeader'
 import { CreateCardModal } from '@/components/cards/modals/createCardModal/CreateCardModal'
 import { DeleteCardModal } from '@/components/cards/modals/deleteCardModal/DeleteCardModal'
 import { EditCardModal } from '@/components/cards/modals/editCardModal/EditCardModal'
+import { DeleteDeckModal } from '@/components/decks/modals/deleteDeckModal/DeleteDeckModal'
+import { EditModal } from '@/components/decks/modals/editModal/EditModal'
 import { Page } from '@/components/page'
-import { Button } from '@/components/ui/button'
 import { Pagination } from '@/components/ui/pagination'
 import { Spinner } from '@/components/ui/spinner'
 import { TextField } from '@/components/ui/textField'
-import { Typography } from '@/components/ui/typography'
 import { useGetDeckCardsQuery, useGetMeQuery, useGetOneDeckQuery } from '@/services'
 
 import s from './deck.module.scss'
@@ -21,6 +22,8 @@ import s from './deck.module.scss'
 export const Deck = () => {
   const { deckId } = useParams()
   const [createMode, setCreateMode] = useState<boolean>(false)
+  const [editDeckMode, setEditDeckMode] = useState<boolean>(false)
+  const [deleteDeckMode, setDeleteDeckMode] = useState<boolean>(false)
   const [cardToDeleteId, setCardToDeleteId] = useState<null | string>(null)
   const [cardToEditId, setCardToEditId] = useState<null | string>(null)
   const {
@@ -49,12 +52,9 @@ export const Deck = () => {
 
   const isOwner = me?.id === deck?.userId
 
-  const toLearnLink = `/decks/${deckId}/learn`
-
   const changeSearchValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
     changeValue(e.currentTarget.value)
   }
-
   const cardToDeleteName = cards?.items?.find(card => card.id === cardToDeleteId)?.question
   const cardToEdit = cards?.items?.find(card => card.id === cardToEditId)
 
@@ -81,18 +81,27 @@ export const Deck = () => {
         onOpenChange={() => setCardToEditId(null)}
         open={!!cardToEditId}
       />
-      <div className={s.header}>
-        <Typography as={'h1'} variant={'h1'}>
-          {isOwner ? 'My Deck' : deck?.name}
-        </Typography>
-        {isOwner && <Button onClick={() => setCreateMode(true)}>Add new card</Button>}
-        {!isOwner && deck && deck.cardsCount > 0 && (
-          <Button as={Link} to={toLearnLink}>
-            Learn to Pack
-          </Button>
-        )}
-      </div>
-      {deck?.cover && <img alt={'deckImg'} className={s.img} src={deck.cover} />}
+      <EditModal
+        id={deckId ?? ''}
+        img={deck?.cover ?? ''}
+        name={deck?.name ?? ''}
+        onOpenChange={() => setEditDeckMode(false)}
+        open={editDeckMode}
+      />
+      <DeleteDeckModal
+        deckName={deck?.name ?? ''}
+        id={deckId ?? ''}
+        onOpenChange={() => setDeleteDeckMode(false)}
+        open={deleteDeckMode}
+      />
+      <CardsHeader
+        deck={deck}
+        deckId={deckId ?? ''}
+        isOwner={isOwner}
+        setCreateMode={setCreateMode}
+        setDeleteDeckMode={setDeleteDeckMode}
+        setEditDeckMode={setEditDeckMode}
+      />
       <TextField
         onChange={changeSearchValueHandler}
         placeholder={'Search cards'}
