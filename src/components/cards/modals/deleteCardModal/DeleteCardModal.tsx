5579993836e958c396d89326cd8e79fedf148692
chepeLove <ilya.chepelov@gmail.com>
2024-02-03 11:15:25 +0300

    fix: change props in component

--- src/components/cards/modals/deleteCardModal/DeleteCardModal.tsx
+++ src/components/cards/modals/deleteCardModal/DeleteCardModal.tsx
@@ -1,51 +1,35 @@
+import { ReactNode } from 'react'
 import { toast } from 'react-toastify'
 
-import { Button } from '@/components/ui/button'
-import { Modal } from '@/components/ui/modal'
+import { DeleteModal } from '@/components/ui/modal/deleteModal'
 import { Typography } from '@/components/ui/typography'
 import { useDeleteCardMutation } from '@/services'
 
-import s from './deleteCardModal.module.scss'
-
 type DeleteCardModalProps = {
-  cardName: string
+  className?: string
   id: string
-  onOpenChange: (open: boolean) => void
-  open: boolean
+  name: string
+  trigger: ReactNode
 }
-export const DeleteCardModal = ({ cardName, id, onOpenChange, open }: DeleteCardModalProps) => {
-  const [deleteCard] = useDeleteCardMutation()
 
-  const closeHandler = () => {
-    onOpenChange(false)
-  }
+export const DeleteCardModal = ({ className, id, name, trigger }: DeleteCardModalProps) => {
+  const [deleteCard, { isLoading }] = useDeleteCardMutation()
 
-  const deleteHandler = async () => {
-    const deleteCardHandler = deleteCard({ id: id }).unwrap()
+  const deleteCardHandler = async () => {
+    await deleteCard({ id: id })
 
-    await toast.promise(deleteCardHandler, {
-      error: 'Failed to delete card',
-      pending: 'Delete card...',
-      success: 'Card delete successfully!',
-    })
-    await deleteCardHandler
-    closeHandler()
+    toast.success('Success updated card')
   }
 
   return (
-    <Modal onOpenChange={onOpenChange} open={open} title={'Delete Card'}>
-      <div className={s.text}>
-        <Typography>Do you really want to remove {cardName}?</Typography>
-        <Typography>All cards will be deleted.</Typography>
-      </div>
-      <div className={s.buttons}>
-        <Button onClick={closeHandler} variant={'secondary'}>
-          Cancel
-        </Button>
-        <Button onClick={deleteHandler} variant={'primary'}>
-          Delete Card
-        </Button>
-      </div>
-    </Modal>
+    <DeleteModal
+      additionalText={<Typography>All cards will be deleted.</Typography>}
+      className={className}
+      deleteItemHandler={deleteCardHandler}
+      disabled={isLoading}
+      name={name}
+      title={'Delete card'}
+      trigger={trigger}
+    />
   )
 }
--- src/components/cards/modals/editCardModal/EditCardModal.tsx
+++ src/components/cards/modals/editCardModal/EditCardModal.tsx
@@ -1,114 +1,38 @@
-import { useEffect, useState } from 'react'
-import { FormProvider, useForm } from 'react-hook-form'
+import { ReactNode, useState } from 'react'
 import { toast } from 'react-toastify'
 
 import { Card } from '@/common/types'
-import { CreateNewAnswer } from '@/components/cards/modals/createCardModal/createNewAnswer/CreateNewAnswer'
-import { CreateNewQuestion } from '@/components/cards/modals/createCardModal/createNewQuestion/CreateNewQuestion'
-import { Button } from '@/components/ui/button'
+import { FormCardModal } from '@/components/cards/modals/formCardModal/FormCardModal'
 import { Modal } from '@/components/ui/modal'
 import { useUpdateCardMutation } from '@/services'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
-
-import s from './editCardModal.module.scss'
 
 type EditCardModalProps = {
-  cardToEdit?: Card
-  id: string | undefined
-  onOpenChange: (open: boolean) => void
-  open: boolean
+  card: Card
+  className?: string
+  trigger: ReactNode
 }
 
-const updateCardSchema = z.object({
-  answer: z.optional(z.string().min(3).max(30)),
-  question: z.optional(z.string().min(3).max(30)),
-})
+export const EditCardModal = ({ card, className, trigger }: EditCardModalProps) => {
+  const [open, setOpen] = useState(false)
+  const [updateCard, { isLoading }] = useUpdateCardMutation()
+  const { id } = card
+  const updateCardHandler = async (data: FormData) => {
+    setOpen(false)
+    await updateCard({ body: data, id })
 
-type EditCardModalPropsFormValues = z.infer<typeof updateCardSchema>
-
-export const EditCardModal = ({ cardToEdit, id, onOpenChange, open }: EditCardModalProps) => {
-  const [newQuestionImg, setNewQuestionImg] = useState<File | null>(null)
-  const [newAnswerImg, setNewAnswerImg] = useState<File | null>(null)
-  const [currentAnswerImg, setCurrentAnswerImg] = useState<string>('')
-  const [currentQuestionImg, setCurrentQuestionImg] = useState<string>('')
-  const [updateCard] = useUpdateCardMutation()
-
-  const methods = useForm<EditCardModalPropsFormValues>({
-    defaultValues: { answer: cardToEdit?.answer ?? '', question: cardToEdit?.question ?? '' },
-    resolver: zodResolver(updateCardSchema),
-  })
-  const { handleSubmit, reset } = methods
-
-  useEffect(() => {
-    setCurrentAnswerImg(cardToEdit?.answerImg ?? '')
-    setCurrentQuestionImg(cardToEdit?.questionImg ?? '')
-    reset({ answer: cardToEdit?.answer, question: cardToEdit?.question })
-  }, [open, cardToEdit, reset])
-
-  const onOpenChangeHandler = () => {
-    onOpenChange(open)
-    reset()
-    setNewQuestionImg(null)
-    setNewAnswerImg(null)
-  }
-
-  const onSubmit = async (data: EditCardModalPropsFormValues) => {
-    const formData = new FormData()
-
-    data.question && formData.append('question', data.question)
-    data.answer && formData.append('answer', data.answer)
-    !currentQuestionImg && formData.append('questionImg', newQuestionImg ?? '')
-    !currentAnswerImg && formData.append('answerImg', newAnswerImg ?? '')
-
-    const updateCardHandler = updateCard({ body: formData, id: id ?? '' }).unwrap()
-
-    await toast.promise(updateCardHandler, {
-      error: 'Failed to update card',
-      pending: 'Update card...',
-      success: 'Card update successfully!',
-    })
-    await updateCardHandler
-    reset()
-    onOpenChange(false)
-  }
-
-  const deleteQuestionImg = () => {
-    setNewQuestionImg(null)
-    setCurrentQuestionImg('')
-  }
-
-  const deleteAnswerImg = () => {
-    setNewAnswerImg(null)
-    setCurrentAnswerImg('')
+    toast.success('Success updated card')
   }
 
   return (
-    <FormProvider {...methods}>
-      <Modal onOpenChange={onOpenChangeHandler} open={open} title={`Edit ${cardToEdit?.question}`}>
-        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-          <CreateNewQuestion
-            currentQuestionImg={currentQuestionImg}
-            deleteQuestionImg={deleteQuestionImg}
-            newQuestionImg={newQuestionImg}
-            setQuestionImg={setNewQuestionImg}
-          />
-          <CreateNewAnswer
-            currentAnswerImg={currentAnswerImg}
-            deleteAnswerImg={deleteAnswerImg}
-            newAnswerImg={newAnswerImg}
-            setAnswerImg={setNewAnswerImg}
-          />
-          <div className={s.buttons}>
-            <Button onClick={onOpenChangeHandler} type={'reset'} variant={'secondary'}>
-              Cancel
-            </Button>
-            <Button type={'submit'} variant={'primary'}>
-              Save Changes
-            </Button>
-          </div>
-        </form>
-      </Modal>
-    </FormProvider>
+    <Modal onOpenChange={setOpen} open={open} title={'Edit Card'} trigger={trigger}>
+      <FormCardModal
+        buttonText={'Save Changes'}
+        card={card}
+        className={className}
+        disabled={isLoading}
+        onSubmit={updateCardHandler}
+        setOpen={setOpen}
+      />
+    </Modal>
   )
 }
