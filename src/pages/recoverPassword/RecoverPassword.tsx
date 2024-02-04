a9e8ec8ffe3816f284b7f992e3bd4ffc49251d95
chepeLove <ilya.chepelov@gmail.com>
2024-02-04 20:24:01 +0300

    feat:add email sending

--- src/pages/recoverPassword/RecoverPassword.tsx
+++ src/pages/recoverPassword/RecoverPassword.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
-import { ROUTES } from '@/common/const'
+import { PASSWORD_RESET_EMAIL_TEMPLATE, ROUTES } from '@/common/const'
 import { ForgotPassword } from '@/components/auth/forgotPassword'
 import { Page } from '@/components/page'
 import { useRecoverPasswordMutation } from '@/services'
@@ -13,18 +13,27 @@ export const RecoverPassword = () => {
 
   const navigate = useNavigate()
 
-  const handleRecoverPassword = async (email: string) => {
+  const recoverPasswordHandler = async (email: string) => {
     try {
-      await recoverPassword({ email }).unwrap()
       navigate(ROUTES.checkEmail, { state: { email } })
+      const recoverPasswordResult = recoverPassword({
+        email,
+        html: PASSWORD_RESET_EMAIL_TEMPLATE,
+      }).unwrap()
+
+      await toast.promise(recoverPasswordResult, {
+        pending: 'Sending email...',
+        success: 'Email sent successfully!',
+      })
+      await recoverPasswordResult
     } catch (error: any) {
-      toast.error(error?.data?.message ?? 'Uncaught Error')
+      toast.error(error?.data?.message ?? 'Error sending email. Please try again.')
     }
   }
 
   return (
     <Page className={s.page}>
-      <ForgotPassword recoverPassword={handleRecoverPassword} />
+      <ForgotPassword recoverPassword={recoverPasswordHandler} />
     </Page>
   )
 }
