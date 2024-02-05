4feda3f8797f103ff077546bbc0b6d5f45bbe3fc
MrUnderSan <MrUnderSan@gmail.com>
2024-02-05 20:45:19 +0300

    feat: add Loader to page

--- src/pages/learn/Learn.tsx
+++ src/pages/learn/Learn.tsx
@@ -5,22 +5,25 @@ import { Grade } from '@/common/types'
 import { LearnCard } from '@/components/learnCard'
 import { Page } from '@/components/page'
 import { BackButton } from '@/components/ui/backButton'
+import { Loader } from '@/components/ui/loader'
 import { useGetOneDeckQuery, useGetRandomCardQuery, useGradeCardMutation } from '@/services'
 
 export const Learn = () => {
   const params = useParams()
   const id = params.id as string
-  const { currentData: deck } = useGetOneDeckQuery({ id })
-  const { currentData: randomCard } = useGetRandomCardQuery({ id })
+  const { currentData: deck, isLoading: deckIsLoading } = useGetOneDeckQuery({ id })
+  const { currentData: randomCard, isLoading: randomCardIsLoading } = useGetRandomCardQuery({ id })
   const [card, setCard] = useState(randomCard)
 
-  const [gradeCard] = useGradeCardMutation()
+  const [gradeCard, { isLoading: gradeCardIsLoading }] = useGradeCardMutation()
 
   useEffect(() => {
     if (randomCard) {
       setCard(randomCard)
     }
   }, [randomCard])
+
+  const isLoading = deckIsLoading || randomCardIsLoading || gradeCardIsLoading
 
   const onSubmit = async (data: Grade, changeRateMode: any) => {
     if (card) {
@@ -43,6 +46,7 @@ export const Learn = () => {
     <Page marginTop={'24px'}>
       <BackButton text={'Back to Decks List'} />
       {deck && card && <LearnCard card={card} deckName={deck.name} onSubmit={onSubmit} />}
+      {isLoading && <Loader />}
     </Page>
   )
 }
