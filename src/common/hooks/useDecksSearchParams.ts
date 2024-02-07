139d0151f24b3fc62cce10e88e363d73cc2b828d
chepeLove <ilya.chepelov@gmail.com>
2024-02-07 15:47:27 +0300

    feat: add getMinMaxCards in hook

--- src/common/hooks/useDecksSearchParams.ts
+++ src/common/hooks/useDecksSearchParams.ts
@@ -1,48 +1,58 @@
-import { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
 import { Sort } from '@/components/tableSortHeader'
+import { useGetMinMaxCardsQuery } from '@/services'
 
 export const useDecksSearchParams = () => {
+  const { data: minMaxCards } = useGetMinMaxCardsQuery()
   const [searchParams, setSearchParams] = useSearchParams()
   const sortKey = searchParams.get('sortKey') || null
   const sortDirection = searchParams.get('sortDirection') || null
   const itemsPerPage = Number(searchParams.get('items')) || 10
-  const minCards = Number(searchParams.get('minCards')) || 0
-  const maxCards = Number(searchParams.get('maxCards')) || 50
+  const minCards = Number(searchParams.get('minCards') || 0)
+  const maxCards = Number(searchParams.get('maxCards') || 61)
   const page = Number(searchParams.get('page')) || 1
   const value = searchParams.get('value')
+  const tabValue = searchParams.get('tab') || 'all'
 
-  const [rangeValue, setRangeValue] = useState<number[]>([minCards, maxCards])
-  const changeMinMaxCard = (values: number[]) => {
-    if (values[0] !== 0 || values[1] !== 50) {
-      searchParams.set('minCards', values[0].toString())
-      searchParams.set('maxCards', values[1].toString())
+  const changeSearchParams = (field: string, params: string) => {
+    if (!params) {
+      searchParams.delete(field)
     } else {
-      searchParams.delete('minCards')
-      searchParams.delete('maxCards')
+      searchParams.set(field, params)
     }
-    setRangeValue(values)
+    searchParams.set('page', '1')
     setSearchParams(searchParams)
+  }
+
+  const changeTabValue = (tabValue: string) => {
+    changeSearchParams('tab', tabValue)
   }
 
   const changePage = (page: number) => {
     searchParams.set('page', page.toString())
     setSearchParams(searchParams)
   }
+
   const changeValue = (value: string) => {
-    if (value) {
-      searchParams.set('value', value)
-    } else {
-      searchParams.delete('value')
+    changeSearchParams('value', value.toString())
+  }
+
+  const changeMinMaxCard = (values: number[]) => {
+    searchParams.set('minCards', values[0].toString())
+    searchParams.set('maxCards', values[1].toString())
+    if (values[0] === minMaxCards?.min) {
+      searchParams.delete('minCards')
+    }
+    if (values[1] === minMaxCards?.max) {
+      searchParams.delete('maxCards')
     }
     searchParams.set('page', '1')
     setSearchParams(searchParams)
   }
+
   const changeItemsPerPage = (items: string) => {
-    searchParams.set('items', items)
-    searchParams.set('page', '1')
-    setSearchParams(searchParams)
+    changeSearchParams('items', items)
   }
 
   const changeSort = (sort: Sort) => {
@@ -54,6 +64,10 @@ export const useDecksSearchParams = () => {
       searchParams.set('sortDirection', sort.direction)
     }
     setSearchParams(searchParams)
+  }
+
+  const clearFilter = () => {
+    setSearchParams({})
   }
 
   const sort: Sort =
@@ -69,13 +83,16 @@ export const useDecksSearchParams = () => {
     changeMinMaxCard,
     changePage,
     changeSort,
+    changeTabValue,
     changeValue,
+    clearFilter,
     itemsPerPage,
     maxCards,
     minCards,
+    minMaxCards,
     page,
-    rangeValue,
     sort,
+    tabValue,
     value,
   }
 }
